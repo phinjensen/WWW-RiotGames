65fbac1ea2eed94bbe921e22a2ec7cab3ff3b3d7
Phin Jensen <phin@endpoint.com>
2013-12-12 21:38:29 +0200

    Added get_champions, get_game_by_id, get_league_by_id

--- lib/RiotGamesAPI.pm
+++ lib/RiotGamesAPI.pm
@@ -7,9 +7,16 @@ use warnings;
 
 use Exporter qw( import );
 
-our @EXPORT_OK = qw( get_summoner_by_name set_region set_api_key );
+our @EXPORT_OK = qw(
+    set_region
+    set_api_key
+    get_champions
+    get_game_by_id
+    get_league_by_id
+    get_summoner_by_name
+);
 
-my $url_base = "http://prod.api.pvp.net/api/lol/";
+my $url_base = "http://prod.api.pvp.net/api/";
 my $region;
 my $api_key;
 
@@ -23,7 +30,26 @@ sub set_api_key {
 
 sub make_lol_api_call {
     my ($lookup) = @_;
-    decode_json( get($base_url . $region . $lookup . "?api_key=$api_key") );
+    decode_json( get($url_base . "lol/" . $region . $lookup . "?api_key=$api_key") );
+}
+
+sub make_api_call {
+    my ($lookup) = @_;
+    decode_json( get($url_base . $region . $lookup . "?api_key=$api_key") );
+}
+
+sub get_champions {
+    make_lol_api_call("/v1.1/champion");
+}
+
+sub get_game_by_id {
+    my ($summonerid) = @_;
+    make_lol_api_call("/v1.1/game/by-summoner/$summonerid/recent");
+}
+
+sub get_league_by_id {
+    my ($summonerid) = @_;
+    make_api_call("/v2.1/league/by-summoner/$summonerid");
 }
 
 sub get_summoner_by_name {
