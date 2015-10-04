07c8a8a8d53858d88bb7559171aa61a93b2ff351
Phin Jensen <phin@zayda.net>
2015-10-03 23:12:20 -0400

    Added url building functions and started champions

--- lib/WWW/RiotGames.pm
+++ lib/WWW/RiotGames.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Moo;
 use HTTP::Tiny;
+use Data::Dumper;
 
 has api_key => (
     is => 'ro',
@@ -19,6 +20,10 @@ has region => (
     },
 );
 
+#
+# Helper functions
+#
+
 sub _is_valid_region {
     my $region = shift;
     return grep(/^$region$/, qw/br eune euw kr lan las na oce ru tr/);
@@ -27,10 +32,37 @@ sub _is_valid_region {
 sub _build_url_base {
     my ($self, $global) = @_;
     if ($global) {
-        return "https://global.api.pvp.net/api/lol/";
+        return "https://global.api.pvp.net/";
     } else {
-        return "https://" . $self->region . ".api.pvp.net/api/lol/";
+        return "https://" . $self->region . ".api.pvp.net/";
     }
+}
+
+sub _build_url {
+    my ($self, $path, $api_version, $lookup, $options) = @_;
+    my $base = $self->_build_url_base();
+    return $base . $path . $self->region . "/$api_version/$lookup" . $self->_build_query_string($options);
+}
+
+sub _build_query_string {
+    my ($self, $options) = @_;
+    my $opt_string = "?api_key=" . $self->api_key;
+    foreach my $key (keys %$options) {
+        $opt_string .= "&$key=$options->{$key}";
+    }
+    return $opt_string;
+}
+
+#
+# Champions
+#
+
+sub get_champions {
+    my ($self, $free_to_play, $api_version) = @_;
+    my %options = (
+        freeToPlay => $free_to_play ? 'true' : 'false',
+    );
+    return $self->_build_url('api/lol/', 'v1.2', 'champion', \%options);
 }
 
 1;
